fcbeb743d96758e322f5c38483028521ac5a6d97
Emanuel Rivera A <riveraaemanuel8418@gmail.com>
2020-05-03 12:02:33 -0600

    Se creó proyecto

---
+++ src/java/servlets/EjemploServlet.java
@@ -0,0 +1,32 @@
+
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "EjemploServlet", urlPatterns = {"/EjemploServlet"})
+public class EjemploServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            
+            PrintWriter out = response.getWriter();
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Ejemplo</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>resspuesta desde un servlet</h1>");
+            out.println("</body>");
+            out.println("</html>");
+            out.close();
+    }
+
+}
